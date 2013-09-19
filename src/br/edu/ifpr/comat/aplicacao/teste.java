6b35e793eb1f336672c092e0ee409b45f3a1a13d
Cristhiano Cardoso <cristhiano@c3info.com.br>
2013-09-19 11:16:35 -0300

    Definição classes básicas

---
+++ src/br/edu/ifpr/comat/aplicacao/teste.java
@@ -0,0 +1,11 @@
+package br.edu.ifpr.comat.aplicacao;
+
+/**
+ * @project comat
+ * @class   teste
+ * @author  Cristhiano Konczak Cardoso <cristhiano@c3info.com.br>
+ * @date    19/09/2013
+ */
+public class teste {
+
+}
--- src/br/edu/ifpr/comat/modelo/Cliente.java
+++ src/br/edu/ifpr/comat/modelo/Cliente.java
@@ -14,4 +14,5 @@ public class Cliente {
     String celular;
     String fax;
     String observacoes;
+    
 }
